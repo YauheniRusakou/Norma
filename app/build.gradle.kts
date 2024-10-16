424fed21d3bba03d3af8efa42814c88059a4c208
Yauheni Rusakou <ya.rusakou@gmail.com>
2024-10-16 16:58:06 +0300

    new version of google ads

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,5 +51,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.2.0-alpha01")
-    implementation("com.google.android.gms:play-services-ads:23.3.0")
+    implementation("com.google.android.gms:play-services-ads:23.4.0")
 }
